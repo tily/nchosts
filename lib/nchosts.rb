95bf074c329531f2aa6f56bf0c8266cdc876cf7b
tily <tidnlyam@gmail.com>
2015-03-06 18:54:49 +0900

    update source

--- lib/nchosts.rb
+++ lib/nchosts.rb
@@ -1,5 +1,8 @@
 require "thor"
 require "ace-client"
+require "ace-client/niftycloud/computing"
+require "json"
+require "erubis"
 
 require "nchosts/version"
 require "nchosts/command"
--- lib/nchosts/command.rb
+++ lib/nchosts/command.rb
@@ -5,24 +5,48 @@ module Nchosts
 			puts "v#{Nchosts::VERSION}"
 		end
 
-		desc 'generate', 'generate hosts info'
-		option :format, :alias => :f
-		option :config, :alias => :c
-		option :ip_type, :alias => :i # :private, :global, defaults to :global
+		desc 'collect', 'collect hosts info from nifty cloud api'
+		option :config, :aliases => :c
+		option :output_path, :aliases => :o
+		def collect
+			instances = []
+			json = JSON.parse File.read(options[:config])
+			json['accounts'].each do |account|
+				client = AceClient::Niftycloud::Computing.build_client(
+					endpoint: 'cp.cloud.nifty.com',
+					path: '/api',
+					access_key_id: account['access_key_id'],
+					secret_access_key: account['secret_access_key']
+				)
+				client.regions.each do |region|
+					client.endpoint = region['regionEndpoint']
+					client.instances.each do |instance|
+						instance['region'] = region
+						instance['account'] = account
+						p instance
+						instances << instance
+					end
+				end
+			end
+			File.write options[:output_path], instances.to_json
+		end
+
+		desc 'generate', 'generate'
+		option :format, :aliases => :f # hosts, capistrano, ssh_config
 		option :include # host name regexp
 		option :exclude # host name regexp
-		option :template # custom erb template
+		option :template, :aliases => :t
+		option :input_path, :aliases => :i
 		def generate
-			instances = []
-			lines = File.read(options[:config]).split(/\n/)
-			lines.each do |line|
-				# TODO: sharp comment for user memo
-				aki, sak = line.split(/\:/)
-				instances += client(aki, sak).instances
-			end
+			template = template(options[:template])
+			instances = JSON.parse File.read options[:input_path]
+			puts Erubis::Eruby.new(template).result(:instances => instances)
 		end
 
 		no_commands do
+			def template(name)
+				File.read File.join(File.dirname(__FILE__), "templates/#{name}.erb")
+			end
 		end
 	end
 end
