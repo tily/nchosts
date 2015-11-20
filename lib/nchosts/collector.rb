26e2386a0400832a18f3b1e02cb7bf76868f6264
tily <tidnlyam@gmail.com>
2015-11-20 11:26:47 +0900

    add endpoint: uscp.cloud.nifty.com (closes #4)

--- lib/nchosts/collector.rb
+++ lib/nchosts/collector.rb
@@ -1,21 +1,30 @@
 module Nchosts
 	class Collector
-		attr_accessor :client
+		ENDPOINTS = %w(
+			cp.cloud.nifty.com
+			uscp.cloud.nifty.com
+		)
+		attr_accessor :clients
 
 		def initialize(account)
-			@client = AceClient::Niftycloud::Computing.build_client(
-				endpoint: 'cp.cloud.nifty.com',
-				path: '/api/',
-				access_key_id: account['access_key_id'],
-				secret_access_key: account['secret_access_key']
-			)
+			@clients = []
+			ENDPOINTS.each do |endpoint|
+				@clients << AceClient::Niftycloud::Computing.build_client(
+					endpoint: endpoint,
+					path: '/api/',
+					access_key_id: account['access_key_id'],
+					secret_access_key: account['secret_access_key']
+				)
+			end
 		end
 
 		def collect
-			client.regions.each do |region|
-				client.endpoint = region['regionEndpoint']
-				client.instances.each do |instance|
-					yield region, instance
+			@clients.each do |client|
+				client.regions.each do |region|
+					client.endpoint = region['regionEndpoint']
+					client.instances.each do |instance|
+						yield region, instance
+					end
 				end
 			end
 		end
